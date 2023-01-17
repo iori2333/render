a46dcab2ce8fa3982c073fd038ec1f1e49f07c18
ZFirozen <zfirozen@163.com>
2023-01-17 17:48:54 +0800

    Fixed import omission.

--- render/objects/__init__.py
+++ render/objects/__init__.py
@@ -1,7 +1,7 @@
 from .container import Container, FixedContainer, JustifyContent
 from .image import Image
 from .relative import RelativeContainer
-from .stack import Stack
+from .stack import CoveredStack, Stack
 from .text import Text
 
 __all__ = [
