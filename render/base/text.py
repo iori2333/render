25653d1dc7998ae8c428ca7978aef2b72376e061
QMY <782719906@qq.com>
2023-01-20 00:03:04 +0800

    feat: support stroke width and color for text

--- render/base/text.py
+++ render/base/text.py
@@ -17,12 +17,16 @@ class RenderText:
         font: PathLike,
         size: int,
         color: Color = Palette.BLACK,
+        stroke_width: int = 0,
+        stroke_color: Optional[Color] = None,
         background: Color = Palette.TRANSPARENT,
     ):
         self.text = text
         self.font = font
         self.size = size
         self.color = color
+        self.stroke_width = stroke_width
+        self.stroke_color = stroke_color
         self.background = background
 
     @classmethod
@@ -32,28 +36,33 @@ class RenderText:
         font: PathLike,
         size: int = 12,
         color: Optional[Color] = None,
+        stroke_width: int = 0,
+        stroke_color: Optional[Color] = None,
         background: Color = Palette.TRANSPARENT,
     ) -> Self:
         if color is None:
             r, g, b = background.to_rgb()
             lightness = 0.2126 * r + 0.7152 * g + 0.0722 * b
             color = Palette.WHITE if lightness < 128 else Palette.BLACK
-        return cls(text, font, size, color, background)
+        return cls(text, font, size, color, stroke_width, stroke_color,
+                   background)
 
     def render(self) -> RenderImage:
         font = ImageFont.truetype(str(self.font), self.size)
-        width, _ = font.getsize(self.text)
+        width, _ = font.getsize(self.text, stroke_width=self.stroke_width)
         ascent, descent = font.getmetrics()
         # height always equals to max possible height of the font
         # instead of actual height of the text for better alignment
-        height = ascent + descent
+        height = ascent + descent + self.stroke_width * 2
         im = Image.new("RGBA", (width, height), self.background)
         draw = ImageDraw.Draw(im)
         draw.text(
-            (0, 0),
-            self.text,
-            self.color,
+            xy=(self.stroke_width, self.stroke_width),
+            text=self.text,
+            fill=self.color,
             font=font,
+            stroke_width=self.stroke_width,
+            stroke_fill=self.stroke_color,
         )
         return RenderImage.from_raw(np.asarray(im))
 
@@ -67,5 +76,11 @@ class RenderText:
     @property
     def width(self) -> int:
         font = ImageFont.truetype(str(self.font), self.size)
-        width, _ = font.getsize(self.text)
+        width, _ = font.getsize(self.text, stroke_width=self.stroke_width)
         return width
+
+    @property
+    def height(self) -> int:
+        font = ImageFont.truetype(str(self.font), self.size)
+        ascent, descent = font.getmetrics()
+        return ascent + descent + self.stroke_width * 2
