ca76e20d533ae243465e1303445bd1bd39cdbe5e
QMY <782719906@qq.com>
2023-02-11 19:46:54 +0800

    fix: fix docstring

--- render/base/object.py
+++ render/base/object.py
@@ -38,6 +38,7 @@ class BaseStyle(TypedDict, total=False):
         | | | +-------+ | | |
         | | +-----------+ | |
         | +---------------+ |
+        +-------------------+
     """
     background: Color
     margin: Space
